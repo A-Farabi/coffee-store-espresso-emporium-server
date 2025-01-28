f54bef7045d38f23dd00eac534d74cb81c053e9d
A-Farabi <farabisyl2017@gmail.com>
2025-01-28 18:50:57 +0600

    update functionlity done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 // middleware
 app.use(cors())
@@ -38,8 +39,45 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/coffee', async(req, res)=>{
+        const cursor = coffeeCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
 
+    app.delete('/coffee/:id', async (req, res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await coffeeCollection.deleteOne(query)
+        res.send(result)
+    })
 
+    app.get('/coffee/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.put('/coffee/:id', async(req, res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const updatedCoffee = req.body
+      const coffee = {
+        $set:{
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options)
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
